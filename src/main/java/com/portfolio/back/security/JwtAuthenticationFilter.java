2241bf3dcc1e9a93c7f820c3284c47292a82afb3
jzkde <dagjjj@gmail.com>
2023-05-26 15:19:59 -0300

    V1.1.4

--- src/main/java/com/portfolio/back/security/JwtAuthenticationFilter.java
+++ src/main/java/com/portfolio/back/security/JwtAuthenticationFilter.java
@@ -12,8 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
